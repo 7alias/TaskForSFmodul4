81e726df73e0274390ff86b8c5ef9ede8d4ed832
7alias <xc.navi@gmail.com>
2021-12-06 11:38:48 +0200

    Initial commit

---
+++ src/Helicopter.java
@@ -0,0 +1,64 @@
+public class Helicopter {
+
+    private int x = 0;
+    private int h = 0;
+    private String name = "";
+    private boolean broken = false;
+
+    private int getX() {
+        return x;
+    }
+
+    private int getH() {
+        return h;
+    }
+
+    private String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void goForward(int x) {
+        this.x += x;
+    }
+
+    public void goBack(int x) {
+        goForward(-x);
+    }
+
+    public void goUp(int h) {
+        this.h += h;
+    }
+
+    public void goDown(int h) {
+
+        goUp(-h);
+        while (this.h <= 0) {
+
+
+            System.out.println("Helicopter " + getName() + " crashed");
+            broken = true;
+            break;
+        }
+
+
+    }
+
+
+    public void setPosition(int h, int x) {
+        this.h = h;
+        this.x = x;
+
+    }
+
+    void printPosition() {
+        if (broken == false) {
+            System.out.println("Helicopter " + getName() + " at the altitude position: " + getH() + " and range: " + getX());
+        } else {
+            System.out.println("Helicopter " + getName() + " is broken.");
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Helicopter helicopter1 = new Helicopter();
+        Helicopter helicopter2 = new Helicopter();
+
+        helicopter1.setPosition(10, 20);
+        helicopter1.setName("Apache");
+        helicopter2.setPosition(20, 30);
+        helicopter2.setName("Chinook");
+        helicopter1.printPosition();
+        helicopter2.printPosition();
+
+helicopter1.goForward(10);
+helicopter1.goUp(10);
+helicopter2.goBack(10);
+helicopter2.goDown(10);
+helicopter2.goDown(10);
+
+        helicopter1.printPosition();
+        helicopter2.printPosition();
+
+    }
+
+
+}
